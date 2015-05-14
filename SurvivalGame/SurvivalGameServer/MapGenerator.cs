a43a05bdd405782b51876e4bff0ac80cbf267f2c
Joell Stehouwer <joell@stehouwer.net>
2015-05-14 19:31:39 +0200

    TREES

--- SurvivalGame/SurvivalGameServer/MapGenerator.cs
+++ SurvivalGame/SurvivalGameServer/MapGenerator.cs
@@ -19,40 +19,42 @@ namespace Mentula.SurvivalGameServer
                 x = i % cSize + pos.X * cSize;
                 y = i / cSize + pos.Y * cSize;
                 float rain = 0;
-                rain += PerlinNoise.Generate(70, cSize * 4, x, y,"1");
-                rain += PerlinNoise.Generate(20, cSize, x, y,"2");
-                rain += PerlinNoise.Generate(10, cSize/4, x, y,"3");
+                rain += PerlinNoise.Generate(70, cSize * 4, x, y, "1");
+                rain += PerlinNoise.Generate(20, cSize, x, y, "2");
+                rain += PerlinNoise.Generate(10, cSize / 4, x, y, "3");
                 float lakeyness = 0;
                 lakeyness += PerlinNoise.Generate(100, cSize / 4, x, y, "lakey");
-                int textureid=-1;
-                if (rain>=0&&rain<25)
+                float chanceToSpawnTree = Math.Max(rain - 15, 0) / 10;
+
+                int textureid = -1;
+                if (rain >= 0 & rain < 25)
                 {
                     textureid = 0;
                 }
-                else if (rain>=25&&rain<50)
+                else if (rain >= 25 & rain < 50)
                 {
                     textureid = 1;
                 }
-                else if (rain>=50&&rain <75)
+                else if (rain >= 50 & rain < 75)
                 {
                     textureid = 2;
                 }
-                else if (rain >=75 && rain <=100)
+                else if (rain >= 75 & rain <= 100)
                 {
                     textureid = 3;
                 }
-                float chanceToSpawnTree = Math.Max(rain - 15, 0) / 10;
-                if (lakeyness>80)
+
+                if (lakeyness > 80)
                 {
-                    destructibles.Add(new Destructible(100, new Tile(new IntVector2(i % cSize, i / cSize),5, 1, true)));
+                    destructibles.Add(new Destructible(100, new Tile(new IntVector2(i % cSize, i / cSize), 5, 1, true)));
                 }
-                else if (RNG.RFloatFromString(x,y)<=chanceToSpawnTree)
+                else if (RNG.RFloatFromString(x, y)*100 <= chanceToSpawnTree)
                 {
-                    destructibles.Add(new Destructible(100, new Tile(new IntVector2(i % cSize,i/cSize),4,1,true)));
+                    destructibles.Add(new Destructible(100, new Tile(new IntVector2(i % cSize, i / cSize), 4, 1, true)));
                 }
-                Tiles[i] = new Tile(new IntVector2(i%cSize, i/cSize), (byte)textureid);
+                Tiles[i] = new Tile(new IntVector2(i % cSize, i / cSize), (byte)textureid);
             }
-            return new Chunk(pos, Tiles,destructibles);
+            return new Chunk(pos, Tiles, destructibles);
         }
     }
 }
