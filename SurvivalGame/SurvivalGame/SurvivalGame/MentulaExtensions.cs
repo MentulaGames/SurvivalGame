3c9671feec98eaa1b6e00ad35a327fc74927ebb5
Frank <largewowfan@gmail.com>
2015-05-18 12:04:56 +0200

    Mouse Pos

--- SurvivalGame/SurvivalGame/SurvivalGame/MentulaExtensions.cs
+++ SurvivalGame/SurvivalGame/SurvivalGame/MentulaExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Mentula.SurvivalGame
 {
@@ -8,6 +9,12 @@ namespace Mentula.SurvivalGame
         public static void Draw(this SpriteBatch batch, Texture2D texture, Vector2 position, Color color, byte layer)
         {
             batch.Draw(texture, position, null, color, 0f, Vector2.Zero, 1f, SpriteEffects.None, 1f / (layer + 1));
+        }
+
+        public static Vector2 GetMousePos()
+        {
+            MouseState s = Mouse.GetState();
+            return new Vector2(s.X, s.Y);
         }
     }
 }
