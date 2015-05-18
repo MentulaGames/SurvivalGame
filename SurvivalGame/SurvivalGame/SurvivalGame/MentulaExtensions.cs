bca9c5dc6ca2a633a7eb110affe3271bb9d45f01
Frank <largewowfan@gmail.com>
2015-05-18 13:29:24 +0200

    Draw rotation

--- SurvivalGame/SurvivalGame/SurvivalGame/MentulaExtensions.cs
+++ SurvivalGame/SurvivalGame/SurvivalGame/MentulaExtensions.cs
@@ -11,6 +11,11 @@ namespace Mentula.SurvivalGame
             batch.Draw(texture, position, null, color, 0f, Vector2.Zero, 1f, SpriteEffects.None, 1f / (layer + 1));
         }
 
+        public static void Draw(this SpriteBatch batch, Texture2D texture, Vector2 position, Color color, float rotation)
+        {
+            batch.Draw(texture, position, null, color, rotation, Vector2.Zero, 1f, SpriteEffects.None, 0);
+        }
+
         public static Vector2 GetMousePos()
         {
             MouseState s = Mouse.GetState();
