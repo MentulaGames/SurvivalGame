fb5740dbc79e6f2bef947dda10413c902d2126fb
Joell Stehouwer <joell@stehouwer.net>
2015-05-23 17:20:35 +0200

    Building Generator

--- SurvivalGame/SurvivalGameServer/Map Generation/BinarySplitGenerator.cs
+++ SurvivalGame/SurvivalGameServer/Map Generation/BinarySplitGenerator.cs
@@ -16,64 +16,79 @@ namespace Mentula.SurvivalGameServer
             List<Rectangle> rectangles = new List<Rectangle>();
             rectangles.Add(new Rectangle(0, 0, bounds.X, bounds.Y));
             bool done = false;
-            while (!done)
+            do
             {
+                done = true;
                 List<Rectangle> rectanglesToSplit = new List<Rectangle>();
                 for (int i = 0; i < rectangles.Count; i++)
                 {
-                    if (rectangles[i].Width < maxSize.X & rectangles[i].Height < maxSize.Y)
+                    
+                    if (rectangles[i].Width > maxSize.X & rectangles[i].Height > maxSize.Y)
                     {
-                        done = true;
+                        done = false;
                     }
-                    else if (rectangles[i].Width > minSize.X * 2 | rectangles[i].Height > minSize.Y * 2)
+                    if (rectangles[i].Width > minSize.X * 2 | rectangles[i].Height > minSize.Y * 2)
                     {
                         rectanglesToSplit.Add(rectangles[i]);
                     }
                 }
-                int ra = (int)Math.Round(r.NextDouble() * rectanglesToSplit.Count);
+                int ra = (int)(r.NextDouble() * rectanglesToSplit.Count);
                 float p = (float)r.NextDouble();
 
                 Rectangle splitting = rectanglesToSplit[ra];
                 bool dir;
-                if (splitting.Width<minSize.X*2)
+                if (splitting.Width < minSize.X * 2)
                 {
                     dir = false;
                 }
-                else if (splitting.Height<minSize.Y*2)
+                else if (splitting.Height < minSize.Y * 2)
                 {
                     dir = true;
                 }
                 else
                 {
-                    dir=Convert.ToBoolean(Math.Round(r.NextDouble()));
+                    dir = Convert.ToBoolean(Math.Round(r.NextDouble()));
                 }
-                float BCorr;
-                if (dir)
-                {
-                    BCorr = -minSize.X * ((p - 0.5f) * 2);
-                }
-                else
-                {
-                    BCorr = -minSize.Y * ((p - 0.5f) * 2);
-                }
+                float BCorr=0;
+                //if (dir)
+                //{
+                //    BCorr = -minSize.X * ((p - 0.5f) * 2);
+                //}
+                //else
+                //{
+                //    BCorr = -minSize.Y * ((p - 0.5f) * 2);
+                //}
+                
                 Rectangle[] sp = new Rectangle[2];
                 if (dir)
                 {
-                    sp[0] = new Rectangle(splitting.X, splitting.Y, (int)Math.Round(BCorr+splitting.Width * p), splitting.Height);
+                    sp[0] = new Rectangle(splitting.X, splitting.Y, (int)Math.Round(BCorr + splitting.Width * p), splitting.Height);
                     sp[1] = new Rectangle(splitting.X + sp[0].Width, splitting.Y, splitting.Width - sp[0].Width, splitting.Height);
                 }
                 else
                 {
-                    sp[0] = new Rectangle(splitting.X, splitting.Y, splitting.Width, (int)Math.Round(BCorr+splitting.Height * p));
+                    sp[0] = new Rectangle(splitting.X, splitting.Y, splitting.Width, (int)Math.Round(BCorr + splitting.Height * p));
                     sp[1] = new Rectangle(splitting.X, splitting.Y + sp[0].Height, splitting.Width, splitting.Height - sp[0].Height);
                 }
+                //if (sp[0].X!=rectangles[ra].X&sp[0].Y!=rectangles[ra].Y)
+                //{
+                //    int dick = 1;
+                //}
                 if (sp[0].Width > minSize.X & sp[0].Height > minSize.Y & sp[1].Width > minSize.X & sp[1].Height > minSize.Y)
                 {
-                    rectanglesToSplit.RemoveAt(ra);
+                    for (int i = 0; i < rectangles.Count; i++)
+                    {
+                        if (rectangles[i].X==sp[0].X&rectangles[i].Y==sp[0].Y)
+                        {
+                            rectangles.RemoveAt(i);
+                        }
+                    }
+
                     rectangles.Add(sp[0]);
                     rectangles.Add(sp[1]);
+
                 }
-            }
+            } while (!done);
             return rectangles;
         }
     }
---
+++ SurvivalGame/SurvivalGameServer/Map Generation/BuildingGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mentula.General;
+using Microsoft.Xna.Framework;
+
+namespace Mentula.SurvivalGameServer
+{
+    public static class BuildingGenerator
+    {
+        public static List<Destructible> GenerateBuilding(IntVector2 pos, IntVector2 size, IntVector2 minRoomSize, IntVector2 maxRoomSize)
+        {
+            List<Destructible> dList = new List<Destructible>();
+            List<Rectangle> binaryMap = BinarySplitGenerator.GenerateBinarySplitMap(size - new IntVector2(1), minRoomSize, maxRoomSize, "a");
+            for (int i = 0; i < binaryMap.Count; i++)
+            {
+                for (int x = 0; x < binaryMap[i].Width; x++)
+                {
+                    for (int y = 0; y < binaryMap[i].Height; y++)
+                    {
+                        IntVector2 desPos = new IntVector2(binaryMap[i].X + x , binaryMap[i].Y + y ) + pos;
+                        if (x == 0 | y == 0)
+                        {
+                            dList.Add(new Destructible(100, desPos, 4, 2, false));
+                        }
+                        else
+                        {
+                            dList.Add(new Destructible(100, desPos, 8, 2, true));
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < size.X; i++)
+            {
+                dList.Add(new Destructible(100, new IntVector2(pos.X + i, pos.Y+size.Y-1), 4, 2, false));
+            }
+            for (int i = 0; i < size.Y-1; i++)
+            {
+                dList.Add(new Destructible(100, new IntVector2(pos.X+size.X-1, pos.Y + i), 4, 2, false));
+            }
+            return dList;
+        }
+    }
+}
