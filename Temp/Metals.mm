5d74b771e66e3a745993506de2c3689f59fee1a0
Frank <largewowfan@gmail.com>
2015-06-01 22:46:55 +0200

    Material loader

---
+++ Temp/Metals.mm
@@ -0,0 +1,4 @@
+[Container: id=0, size=0, name="Metals"]
+{
+	[Item: id=0, name="Gray_Iron", UTS=140, TSAY=98, Density=7.2, MeltingPoint=1808.15, VaporizationPoint=3023.15, IonizationPoint=91290]
+}
