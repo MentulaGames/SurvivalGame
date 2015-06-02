2b98f4768717f9a754cd0e61041c16c14c7b8990
Frank <largewowfan@gmail.com>
2015-06-02 15:50:36 +0200

    More metals

--- SurvivalGame/SurvivalGameServer/SurvivalGameServerContent/Metals.mm
+++ SurvivalGame/SurvivalGameServer/SurvivalGameServerContent/Metals.mm
@@ -1,4 +1,8 @@
 [Container: id=0, size=0, name="Metal"]
 {
-	[Item: id=0, name="Gray_Iron", UTS=140, TSAY=98, density=7.2, meltingPoint=1808.15, vaporizationPoint=3023.15, ionizationPoint=91290]
+	[Item: id=0, name="Iron", UTS=350, TSAY=80, density=7900, meltingPoint=1808, vaporizationPoint=3023, ionizationPoint=91290]
+	[Item: id=1, name="Tin", UTS=15, TSAY=9, density=7300, meltingPoint=505, vaporizationPoint=2875, ionizationPoint=85225]
+	[Item: id=2, name="Copper", UTS=210, TSAY=117, density=8900, meltingPoint=1357, vaporizationPoint=2835, ionizationPoint=89663]
+	[Item: id=3, name="Gold", UTS=100, TSAY=100, density=19300, meltingPoint=1337, vaporizationPoint=3243, ionizationPoint=107055]
+	[Item: id=4, name="Osmium", UTS=1000, TSAY=1000, density=22590, meltingPoint=3306, vaporizationPoint=5285, ionizationPoint=101000]
 }
