77bb5dac6a1ad3c8e4a2400ae85dea9e178f8934
Joell Stehouwer <joell@stehouwer.net>
2015-06-02 22:50:41 +0200

    eee

--- SurvivalGame/SurvivalGameServer/SurvivalGameServer/ImpactObject.cs
+++ SurvivalGame/SurvivalGameServer/SurvivalGameServer/ImpactObject.cs
@@ -10,17 +10,28 @@ namespace Mentula.SurvivalGameServer
 {
     public class ImpactObject
     {
-        public float Velocity;
-        public float Weight;
-        public float Volume;
-        public float ContactArea;
+        public Material Matter;
+        public float Velocity { get; private set; }
+        public float Weight { get; private set; }
+        public float Volume { get; private set; }
+        public float ContactArea { get; private set; }
+        public float E_k { get; private set; }
+        public float MPa { get; private set; }
 
         public ImpactObject(Material m, float velocity, float volume,float contactArea)
         {
+            Matter = m;
             Velocity = velocity;
             Weight = m.Density * volume;
             Volume = volume;
             ContactArea = contactArea;
+            E_k = Weight * velocity * velocity / 2;
+            MPa = E_k / contactArea /100;
+        }
+        public void SetMPa(float MPa)
+        {
+            this.MPa = MPa;
+            E_k = MPa * ContactArea * 100;
         }
     }
 }
