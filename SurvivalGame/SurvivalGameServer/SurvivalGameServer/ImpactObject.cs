5f6fe16917e701974d427cef777d5987d3fa1b6a
Joell Stehouwer <joell@stehouwer.net>
2015-06-02 16:17:14 +0200

    d

---
+++ SurvivalGame/SurvivalGameServer/SurvivalGameServer/ImpactObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mentula.Content;
+
+
+namespace Mentula.SurvivalGameServer
+{
+    public class ImpactObject
+    {
+        public float Velocity;
+        public float Weight;
+        public float Volume;
+        public float ContactArea;
+
+        public ImpactObject(Material m, float velocity, float volume,float contactArea)
+        {
+            Velocity = velocity;
+            Weight = m.Density * volume;
+            Volume = volume;
+            ContactArea = contactArea;
+        }
+    }
+}
---
+++ SurvivalGame/SurvivalGameServer/SurvivalGameServer/MaterialLayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mentula.Content;
+
+namespace Mentula.SurvivalGameServer
+{
+    public class MaterialLayer
+    {
+        public Material Matter;
+        public float Thickness;
+        public readonly float Area;
+        public float CurrArea;
+        public MaterialLayer(Material m, float thickness, float area)
+        {
+            Matter = m;
+            Thickness = thickness;
+            Area = area;
+            CurrArea = area;
+        }
+    }
+}
